41265280f2329e191b73b073b91c9ff086e546c0
Antotor45 <antonindosouto@gmail.com>
2021-12-07 10:48:00 +0100

    Turn Right Large

--- motor/Motors.h
+++ motor/Motors.h
@@ -1,21 +1,36 @@
 #include "motor.h"
 class Motors {
 
-    Motor left;
-    Motor right;
+  Motor left;
+  Motor right;
 
-  public:
-    void setup() {
-      
-      left = Motor();
-      right = Motor();
-    }
+public:
+  void setup() {
+    left = Motor();
+    right = Motor();
+  }
 
-    void advance(int actualSpeed) {
-      left.advance(actualSpeed);
-    }
+  void advance(int actualSpeed) {
+    left.advance(actualSpeed);
+    right.advance(actualSpeed);
+  }
 
-    int getSpeed() {
-      return left.getSpeed();
+  void goBack(int actualSpeed) {
+    left.goBack(actualSpeed);
+    right.goBack(actualSpeed);
+  }
+
+  void turnRightLarge(int actualSpeed) {
+    left.advance(actualSpeed);
+    if (actualSpeed > 20) {
+      right.advance(actualSpeed - 20);
     }
+    else {
+      right.stop();
+    }
+  }
+
+  int getSpeed() {
+    return left.getSpeed();
+  }
 };
--- motor/motor.h
+++ motor/motor.h
@@ -1,54 +1,59 @@
 class Motor {
 
-    int i1;
-    int i2;
-    int EA;
+  int i1;
+  int i2;
+  int EA;
 
-    int actualSpeed = 0;
+  int actualSpeed = 0;
 
-  public:
-    void setup(int pinI1, int pinI2, int pinEA) {
-      i1 = pinI1;
-      i2 = pinI2;
-      EA = pinEA;
-      pinMode(i1, OUTPUT);
-      pinMode(i2, OUTPUT);
-      pinMode(EA, OUTPUT);
-      pinMode(i1, LOW);
-      pinMode(i2, LOW);
-    }
+public:
+  void setup(int pinI1, int pinI2, int pinEA) {
+    i1 = pinI1;
+    i2 = pinI2;
+    EA = pinEA;
+    pinMode(i1, OUTPUT);
+    pinMode(i2, OUTPUT);
+    pinMode(EA, OUTPUT);
+    pinMode(i1, LOW);
+    pinMode(i2, LOW);
+  }
 
-    void setWheelSpeed(int newSpeed) {
-      actualSpeed = newSpeed;
-    }
+  void setWheelSpeed(int newSpeed) {
+    actualSpeed = newSpeed;
+  }
 
-    void advance() {
-      analogWrite(EA, actualSpeed);
-      digitalWrite(i1, LOW);
-      digitalWrite(i2, HIGH);
-    }
+  void advance() {
+    analogWrite(EA, actualSpeed);
+    digitalWrite(i1, LOW);
+    digitalWrite(i2, HIGH);
+  }
 
-    void advance(int newSpeed) {
-      actualSpeed = newSpeed;
-      analogWrite(EA, actualSpeed);
-      digitalWrite(i1, LOW);
-      digitalWrite(i2, HIGH);
-    }
+  void advance(int newSpeed) {
+    actualSpeed = newSpeed;
+    analogWrite(EA, actualSpeed);
+    digitalWrite(i1, LOW);
+    digitalWrite(i2, HIGH);
+  }
 
-    void goBack() {
-      analogWrite(EA, actualSpeed);
-      digitalWrite(i1, HIGH);
-      digitalWrite(i2, LOW);
-    }
+  void goBack() {
+    analogWrite(EA, actualSpeed);
+    digitalWrite(i1, HIGH);
+    digitalWrite(i2, LOW);
+  }
 
-    void goBack(int newSpeed) {
-      actualSpeed = newSpeed;
-      analogWrite(EA, actualSpeed);
-      digitalWrite(i1, HIGH);
-      digitalWrite(i2, LOW);
-    }
+  void goBack(int newSpeed) {
+    actualSpeed = newSpeed;
+    analogWrite(EA, actualSpeed);
+    digitalWrite(i1, HIGH);
+    digitalWrite(i2, LOW);
+  }
 
-    int getSpeed() {
-      return actualSpeed;
-    }
+  void stop(){
+    digitalWrite(i1, LOW);
+    digitalWrite(i2, LOW);
+  }
+
+  int getSpeed() {
+    return actualSpeed;
+  }
 };
